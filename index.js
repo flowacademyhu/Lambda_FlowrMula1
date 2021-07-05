c7a7e0aa48c0657d32eba5595f9d50d921014bce
PatocskaiErik <78388280+PatocskaiErik@users.noreply.github.com>
2021-07-05 16:29:33 +0200

    Added table

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const Player = {
-  coordinates: [[height], [width]]
-};
+// const Player = {
+//   coordinates: [[height], [width]]
+// };
+const table = require('table').table;
 
 const generateMap = (width, height) => {
   const arr = new Array(height);
@@ -18,4 +19,9 @@ const fillMap = (arr) => {
   }
 };
 
-const printGame = (player, enemy, map) => {};
+// const printGame = (player, enemy, map) => {};
+
+const map = generateMap(5, 5);
+fillMap(map);
+// console.log(map);
+console.log(table(map));
