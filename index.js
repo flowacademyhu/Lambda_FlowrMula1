f239906b798ce2c7e5f44b78b73e70dce97ee193
PatocskaiErik <78388280+PatocskaiErik@users.noreply.github.com>
2021-07-05 16:15:51 +0200

    Added GenerateMap

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const Player = {
-  coordinates: [[x], [y]]
+  coordinates: [[height], [width]]
 };
+
+const generateMap = (width, height) => {
+  const arr = new Array(height);
+  for (let i = 0; i < height; i++) {
+    arr[i] = new Array(width);
+  }
+  return arr;
+};
+
+const printGame = (player, enemy, map) => {};
