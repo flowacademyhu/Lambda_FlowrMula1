91d79af3041f29a1ee1bd131d8d0ca3dbdcc8621
Attila Csanyi <csanyiattila23@gmail.com>
2021-07-06 09:24:47 +0200

    Remove fillMap function

--- index.js
+++ index.js
@@ -66,13 +66,3 @@ const printGame = (player, enemies) => {
 };
 
 printGame(player, enemies);
-
-// const fillMap = (arr) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr[i].length; j++) {
-//       if (arr[i][j] !== '*') {
-//         arr[i][j] = ' ';
-//       }
-//     }
-//   }
-// };
