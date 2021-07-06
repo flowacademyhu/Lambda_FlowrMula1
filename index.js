fd1451a4030498fd558381876508c64ea1970fd8
Attila Csanyi <csanyiattila23@gmail.com>
2021-07-06 09:21:58 +0200

    Modify printGame function

--- index.js
+++ index.js
@@ -1,29 +1,78 @@
-// const Player = {
-//   coordinates: [[height], [width]]
-// };
 const table = require('table').table;
-const mapWidth = 5;
-const mapHeight = 5;
 
-const generateMap = (width, height) => {
-  const arr = new Array(height);
-  for (let i = 0; i < height; i++) {
-    arr[i] = new Array(width);
-  }
-  return arr;
+const mapWidth = 10;
+const mapHeight = 10;
+
+const player = {
+  coordinates: [
+    { x: 1, y: mapHeight - 4 },
+    { x: 0, y: mapHeight - 3 },
+    { x: 1, y: mapHeight - 3 },
+    { x: 2, y: mapHeight - 3 },
+    { x: 1, y: mapHeight - 2 },
+    { x: 0, y: mapHeight - 1 },
+    { x: 1, y: mapHeight - 1 },
+    { x: 2, y: mapHeight - 1 }
+  ]
 };
 
-const fillMap = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      arr[i][j] = ' ';
-    }
+const enemies = [
+  {
+    coordinates: [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 1, y: 1 },
+      { x: 0, y: 2 },
+      { x: 1, y: 2 },
+      { x: 2, y: 2 },
+      { x: 1, y: 3 }
+    ]
+  }
+];
+
+const generateMap = (width, height) => {
+  const row = ' '.repeat(width).split('');
+  const map = [];
+  for (let i = 0; i < height; i++) {
+    map.push([...row]);
+  }
+  return map;
+};
+
+const printCar = (car, map) => {
+  for (const coord of car.coordinates) {
+    map[coord.y][coord.x] = '*';
+  }
+  return map;
+};
+
+const printPlayerCar = (player, map) => {
+  printCar(player, map);
+};
+
+const printEnemyCars = (enemies, map) => {
+  for (const enemy of enemies) {
+    printCar(enemy, map);
   }
 };
 
 // const printGame = (player, enemy, map) => {};
+const printGame = (player, enemies) => {
+  const map = generateMap(mapWidth, mapHeight);
+  printPlayerCar(player, map);
+  printEnemyCars(enemies, map);
+  console.log(table(map));
+};
 
-const map = generateMap(mapWidth, mapHeight);
-fillMap(map);
-// console.log(map);
-console.log(table(map));
+printGame(player, enemies);
+
+// const fillMap = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       if (arr[i][j] !== '*') {
+//         arr[i][j] = ' ';
+//       }
+//     }
+//   }
+// };
