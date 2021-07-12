85cb787943bbb3b47b57e4afd21f1fb259917f94
Szabó Gábor <91szabogabor@gmail.com>
2021-07-12 17:01:34 +0200

    Added intro

---
+++ intro.js
@@ -0,0 +1,20 @@
+const CFonts = require('cfonts');
+
+CFonts.say('Team Mate Studio|presents...', {
+  font: 'block', // define the font face
+  align: 'center', // define text alignment
+  colors: ['red', 'black'] // define all colors
+});
+
+CFonts.say('Flowrmula 1!', {
+  font: 'block', // define the font face
+  align: 'center', // define text alignment
+  colors: ['red', 'black'] // define all colors
+});
+
+CFonts.say('Press enter to continue...', {
+  font: 'tiny',
+  align: 'center'
+});
+
+// exports.intro = intro;
