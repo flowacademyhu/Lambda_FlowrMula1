12fb24cc78b11b2d194759f45a419c6b8c8033ac
Szabó Gábor <91szabogabor@gmail.com>
2021-07-12 13:47:24 +0200

    Added menu

--- menu.js
+++ menu.js
@@ -1,14 +1,16 @@
 const term = require('terminal-kit').terminal;
 const ctx = require('axel');
 const index = require('./index');
+const colorPicking = require('./colorPicking');
 
 term.red('Welcome to Flowrmula 1!\n Buckle in!\n');
 
 const items = ['New game', 'High score', 'Exit'];
 
 const menu = term.singleColumnMenu(items, function (_error, response) {
+  console.clear();
   if (response.selectedText === 'New game') {
-    index.startGame();
+    colorPicking.colorPicking();
   }
   // if (response.selectedText === 'High score')
   if (response.selectedText === 'Exit') {
