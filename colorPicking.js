c0316aa72f74a17855ef3954b3f203005e41835b
Szabó Gábor <91szabogabor@gmail.com>
2021-07-12 13:46:53 +0200

    Added colorPicking

---
+++ colorPicking.js
@@ -0,0 +1,25 @@
+const term = require('terminal-kit').terminal;
+const index = require('./index');
+
+const colorPicking = () => {
+  console.clear();
+  term.red('Please, pick a color for your car.\n');
+
+  const items = ['Red', 'Blue', 'Green'];
+
+  term.singleColumnMenu(items, function (_error, response) {
+    if (response.selectedText === 'Red') {
+      index.startGame();
+    }
+    if (response.selectedText === 'Blue') {
+      index.startGame();
+    }
+    if (response.selectedText === 'Green') {
+      index.startGame();
+    }
+  });
+};
+
+exports.colorPicking = colorPicking;
+
+// const colorPicking =
