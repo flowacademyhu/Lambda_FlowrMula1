cc984954cf5643cb5118bed9e673d87e40064bcb
Erik <patoerik93@gmail.com>
2021-07-13 11:03:03 +0200

    fileToString

--- fs.js
+++ fs.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
-
-fs.readFile('./scores.txt', 'utf-8', function (err, data) {
+let array = [];
+fs.readFileSync('./scores.txt', 'utf-8', function (err, data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+  //   array = data;
+  //   return array;
 });
+let fileToString = fs.readFileSync('./scores.txt').toString('utf-8');
+array = fileToString;
+
+console.log(array);
+
+array = array.split('\n');
+array = array.map((index) => index.split(';'));
+array = array.map((elem) => [elem[0], Number(elem[1])]);
+array.sort((a, b) => b[1] - a[1]);
+console.log(array);
