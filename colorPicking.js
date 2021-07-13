80095f425f3242648ec609a3d42d6dc3189b56a4
Szabó Gábor <91szabogabor@gmail.com>
2021-07-13 11:12:38 +0200

    Improved design

--- colorPicking.js
+++ colorPicking.js
@@ -1,23 +1,66 @@
 const term = require('terminal-kit').terminal;
 const addName = require('./addName');
+const CFonts = require('cfonts');
 
 const colorPicking = () => {
   console.clear();
-  term.red('Please, pick a color for your car.\n');
 
-  const items = ['Red', 'Blue', 'Green'];
+  CFonts.say('Please, pick a color for your car.', {
+    font: 'tiny',
+    align: 'center',
+    colors: ['yellow', 'black']
+  });
+
+  const items = [
+    CFonts.render('Red', {
+      font: 'tiny',
+      align: 'center',
+      colors: ['red', 'black']
+    }).string,
+    CFonts.render('Blue', {
+      font: 'tiny',
+      align: 'center',
+      colors: ['red', 'black']
+    }).string,
+    CFonts.render('Green', {
+      font: 'tiny',
+      align: 'center',
+      colors: ['red', 'black']
+    }).string
+  ];
 
   term.singleColumnMenu(items, function (_error, response) {
-    if (response.selectedText === 'Red') {
+    if (
+      response.selectedText ===
+      CFonts.render('Red', {
+        font: 'tiny',
+        align: 'center',
+        colors: ['red', 'black']
+      }).string
+    ) {
       addName.addName();
     }
-    if (response.selectedText === 'Blue') {
+    if (
+      response.selectedText ===
+      CFonts.render('Blue', {
+        font: 'tiny',
+        align: 'center',
+        colors: ['red', 'black']
+      }).string
+    ) {
       addName.addName();
     }
-    if (response.selectedText === 'Green') {
+    if (
+      response.selectedText ===
+      CFonts.render('Green', {
+        font: 'tiny',
+        align: 'center',
+        colors: ['red', 'black']
+      }).string
+    ) {
       addName.addName();
     }
   });
 };
 
-exports.colorPicking = colorPicking;
+module.exports = { colorPicking };
