0ba5e4ec14cf765f45b0139fb7c881fb28209c6a
Szabó Gábor <91szabogabor@gmail.com>
2021-07-13 11:12:22 +0200

    Improved design

--- menu.js
+++ menu.js
@@ -1,19 +1,56 @@
 const term = require('terminal-kit').terminal;
 const colorPicking = require('./colorPicking');
+const CFonts = require('cfonts');
 
 console.clear();
 
-term.red('Welcome to Flowrmula 1!\nBuckle in!\n');
+CFonts.say('Welcome to Flowrmula 1!\n Buckle in.', {
+  font: 'tiny',
+  align: 'center',
+  gradient: ['yellow', 'red']
+});
 
-const items = ['New game', 'High score', 'Exit'];
+const items = [
+  CFonts.render('New game', {
+    font: 'tiny',
+    align: 'center',
+    colors: ['red', 'black']
+  }).string,
+  CFonts.render('High score', {
+    font: 'tiny',
+    align: 'center',
+    colors: ['red', 'black']
+  }).string,
+  CFonts.render('Exit', {
+    font: 'tiny',
+    align: 'center',
+    colors: ['red', 'black']
+  }).string
+];
 
 const menu = term.singleColumnMenu(items, function (_error, response) {
   console.clear();
-  if (response.selectedText === 'New game') {
+  if (
+    response.selectedText ===
+    CFonts.render('New game', {
+      font: 'tiny',
+      align: 'center',
+      colors: ['red', 'black']
+    }).string
+  ) {
     colorPicking.colorPicking();
   }
   // if (response.selectedText === 'High score')
-  if (response.selectedText === 'Exit') {
+  if (
+    response.selectedText ===
+    CFonts.render('Exit', {
+      font: 'tiny',
+      align: 'center',
+      colors: ['red', 'black']
+    }).string
+  ) {
     process.exit();
   }
 });
+
+module.exports = { menu };
