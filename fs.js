a0c92db9b2c7c70480f311841f3b4bb7595d60a5
Erik <patoerik93@gmail.com>
2021-07-13 14:17:59 +0200

    addedHighscore

--- fs.js
+++ fs.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const fonts = require('cfonts');
+const Cfonts = require('cfonts');
 const terminalKit = require('terminal-kit').terminal;
 let array = [];
 fs.readFileSync('./scores.txt', 'utf-8', function (err, data) {
@@ -20,17 +20,33 @@ array = array.slice(0, 5);
 
 // console.log(cfonts "Leaderboard" --font "chrome");
 
-terminalKit.table(array, {
-  hasBorder: true,
-  contentHasMarkup: true,
-  borderChars: 'heavyRounded',
-  borderAttr: { color: 'white' },
-  textAttr: { bgColor: 'default' },
-  firstCellTextAttr: { bgColor: 'black' },
-  firstRowTextAttr: { bgColor: 'black' },
-  firstColumnTextAttr: { bgColor: 'black' },
-  secondColumnTextAttr: { bgColor: 'red' },
-  width: 18,
-  lineWrap: false,
-  fit: true
+// terminalKit.table(array, {
+//   hasBorder: true,
+//   contentHasMarkup: true,
+//   borderChars: 'heavyRounded',
+//   borderAttr: { color: 'white' },
+//   textAttr: { bgColor: 'default' },
+//   firstCellTextAttr: { bgColor: 'black' },
+//   firstRowTextAttr: { bgColor: 'black' },
+//   firstColumnTextAttr: { bgColor: 'black' },
+//   secondColumnTextAttr: { bgColor: 'red' },
+//   width: 18,
+//   lineWrap: false,
+//   fit: true
+// });
+
+Cfonts.say('Leaderboard', {
+  font: 'tiny',
+  align: 'center',
+  letterSpacing: 3,
+  colors: ['yellow', 'black']
 });
+
+Cfonts.say(
+  `${array[0]}\n ${array[1]}\n ${array[2]}\n ${array[3]}\n ${array[4]}`,
+  {
+    font: 'tiny',
+    align: 'center',
+    colors: ['red', 'black']
+  }
+);
