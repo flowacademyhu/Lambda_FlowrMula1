3731104adf3f6919bb43a5070ce1e40ac9b13027
Erik <patoerik93@gmail.com>
2021-07-13 11:54:13 +0200

    row

--- fs.js
+++ fs.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const fonts = require('cfonts');
+const terminalKit = require('terminal-kit').terminal;
 let array = [];
 fs.readFileSync('./scores.txt', 'utf-8', function (err, data) {
   if (err) {
@@ -10,10 +12,25 @@ fs.readFileSync('./scores.txt', 'utf-8', function (err, data) {
 let fileToString = fs.readFileSync('./scores.txt').toString('utf-8');
 array = fileToString;
 
-console.log(array);
-
 array = array.split('\n');
 array = array.map((index) => index.split(';'));
 array = array.map((elem) => [elem[0], Number(elem[1])]);
-array.sort((a, b) => b[1] - a[1]);
-console.log(array);
+array = array.sort((a, b) => b[1] - a[1]);
+array = array.slice(0, 5);
+
+// console.log(cfonts "Leaderboard" --font "chrome");
+
+terminalKit.table(array, {
+  hasBorder: true,
+  contentHasMarkup: true,
+  borderChars: 'heavyRounded',
+  borderAttr: { color: 'white' },
+  textAttr: { bgColor: 'default' },
+  firstCellTextAttr: { bgColor: 'black' },
+  firstRowTextAttr: { bgColor: 'black' },
+  firstColumnTextAttr: { bgColor: 'black' },
+  secondColumnTextAttr: { bgColor: 'red' },
+  width: 18,
+  lineWrap: false,
+  fit: true
+});
