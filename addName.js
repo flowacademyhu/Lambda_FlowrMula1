2d83a6e353b227cdad4456b81a3d315593d05e8f
Szabó Gábor <91szabogabor@gmail.com>
2021-07-14 09:39:45 +0200

    Fixed crashing

--- addName.js
+++ addName.js
@@ -1,12 +1,12 @@
 const term = require('terminal-kit').terminal;
-const index = require('./index');
 const CFonts = require('cfonts');
+const index = require('./index');
 
 const addName = () => {
   console.clear();
 
   CFonts.say(
-    'Please, enter your name.\n After pressing Enter, the game will start.',
+    'Please, enter your name.\n After pressing ENTER, the game will start.',
     {
       font: 'tiny',
       align: 'center',
@@ -16,6 +16,9 @@ const addName = () => {
 
   term.inputField(function (_error, input) {
     index.player.name = input;
+    if (index.player.name === '') {
+      index.player.name = 'Player1';
+    }
     index.startGame();
   });
 };
